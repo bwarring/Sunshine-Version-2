174e933913dd1012543b457613e99862c81e57c8
Bob Warring <bobwarring@contrasoftware.com>
2017-09-06 06:22:50 -0500

    br004-CleanupCommentedCode
    Deleted unnecessary comments.

--- app/src/main/java/com/warassoc/app/activity/MainActivity.java
+++ app/src/main/java/com/warassoc/app/activity/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.Selection;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -58,13 +57,6 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             placeholderFragment = new PlaceholderFragment();
-            //Bundle bundle=new Bundle();
-            //bundle.putSerializable("zipCodeText", (Serializable)zipCodeText.);
-            //bundle.putSerializable("zipCodeTextView", (Serializable)zipCodeTextView);
-            //bundle.putSerializable("numberOfDaysText", (Serializable)numberOfDaysText);
-            //bundle.putSerializable("numberOfDaysTextView", (Serializable)numberOfDaysTextView);
-            //bundle.putSerializable("headerImageView", (Serializable)headerImageView);
-            //placeholderFragment.setArguments(new Bundle());
             getSupportFragmentManager().beginTransaction()
                     .add(container, placeholderFragment)
                     .commit();
@@ -142,11 +134,9 @@ public class MainActivity extends AppCompatActivity {
         request.setType("forecast");
         request.setFrequency("daily");
         request.setLocationZip(zipCode);
-        //request.setLocationZip("66061");
         request.setMode("json");
         request.setUnits("metric");
         request.setCount(count);
-        //request.setCount("7");
         OpenWeatherServiceImpl openWeatherService = new OpenWeatherServiceImpl(b.getContext(), b.getRootView(), request);
         openWeatherService.execute();
     }
@@ -203,132 +193,9 @@ public class MainActivity extends AppCompatActivity {
 
                     Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
-/*
-                    PlaceholderFragment.headerImageView.setVisibility(ImageView.VISIBLE);
-
-                    String zipCode=zipCodeText.getText().toString();
-                    zipCodeText.setVisibility(TextView.VISIBLE);
-                    zipCodeTextView.setVisibility(TextView.VISIBLE);
-
-                    String count=numberOfDaysText.getText().toString();
-                    numberOfDaysText.setVisibility(TextView.VISIBLE);
-                    numberOfDaysTextView.setVisibility(TextView.VISIBLE);
-                    */
                 }
             });
-            // my <code>
-//            EditText et = (EditText)rootView.findViewById(R.id.dateNow);
-//            if (et != null){
-//                et.setText(DateUtility.dateNowStr());
-//                et.setTypeface(Typeface.SERIF);
-//            }
-            // </code>
-
-            //String[] days = new String[]{"Today", "Tomorrow", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday"};
-            //String[] weather = new String[]{"Sunny", "Sunny", "Rainy", "Rainy", "Cloudy", "Cloudy", "Sunny", "Sunny", "Sunny"};
-
-            //List<Forecast> forecastList = new ArrayList<>();
-            //int cnt = 9;
-            //Forecast forecast = null;
-            //for (int i = 0; i < cnt; i++) {
-            //    forecastList.add(new Forecast(days[i], weather[i], RandomUtility.random(75, 85), RandomUtility.random(60, 70)));
-            //}
-
-            // custom array adapter method
-            //ForecastListArrayAdapter adapter = new ForecastListArrayAdapter(this.getContext(), Forecast.list());
-            // attach the adapter to forecast ListView
-            //ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast);
-            //listView.setAdapter(adapter);
-
-            // these two need to be declared outside the try/catch block
-            // so they can be closed in the finally block
-            //HttpURLConnection urlConnection = null;
-            //BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            //String forecastJsonStr = null;
-            try {
-                // Construct URL for the OpenWeatherMap query
-                // Possible parameters are available at OWM's forecast API page at
-                // http://openweathermap.com/API#forcast
-
-                //APPID ="2be6bb1f8190f9079a8d5270be7417c0";
-                //URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=66061&mode=json&units=metric&cnt=7&APPID=2be6bb1f8190f9079a8d5270be7417c0");
-
-                // Create the request to OpenWeatherMap, and open the connection
-                //urlConnection = (HttpURLConnection) url.openConnection();
-                //urlConnection.setRequestMethod("GET");
-                //urlConnection.connect();
-                // call service
-                /*
-                OpenWeatherServiceRequest request = new OpenWeatherServiceRequest();
-                request.setBaseUrl("http://api.openweathermap.org/data/2.5");
-                request.setType("forecast");
-                request.setFrequency("daily");
-                request.setLocationZip("66061");
-                request.setMode("json");
-                request.setUnits("metric");
-                request.setCount("7");
-                OpenWeatherServiceImpl openWeatherService = new OpenWeatherServiceImpl(this.getContext(), rootView, request);
-                openWeatherService.execute();
-                */
-            } catch (Exception e) {
-                Log.e("PlaceholderFragment", "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                //forecastJsonStr = null;
-            }
             return rootView;
-/*
-                //String json = openWeatherService.retrieve(request);
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    forecastJsonStr = null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    forecastJsonStr = null;
-                }
-                forecastJsonStr = buffer.toString();
-            } catch (IOException e) {
-                Log.e("PlaceholderFragment", "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                forecastJsonStr = null;
-            } catch (Exception e) {
-                Log.e("PlaceholderFragment", "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attempting
-                // to parse it.
-                forecastJsonStr = null;
-            }
-            finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e("PlaceholderFragment", "Error closing stream", e);
-                }
-            }
-
-                return rootView;
-            }
-            */
         }
     }
 }
